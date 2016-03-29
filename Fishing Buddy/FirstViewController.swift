50f8eb04a8f2d7ed2d9472f6072b135349973139
edballington <eballington@gmail.com>
2016-03-29 09:37:14 -0400

    Initial Commit

---
+++ Fishing Buddy/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Fishing Buddy
+//
+//  Created by Ed Ballington on 3/29/16.
+//  Copyright © 2016 Ed Ballington. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Fishing Buddy/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Fishing Buddy
+//
+//  Created by Ed Ballington on 3/29/16.
+//  Copyright © 2016 Ed Ballington. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
